1a7c5bb70e8163367c4955f5b1011ebc0597ecd8
Luiz Casara <luiz.casara@maxxidata.com>
2022-05-15 13:24:25 -0300

    fix: fix proptype

--- src/components/Input/InputText.js
+++ src/components/Input/InputText.js
@@ -10,6 +10,6 @@ export default function InputText({onChange, placeholder}) {
 }
 
 InputText.propTypes = {
-    onChange: PropTypes.object,
+    onChange: PropTypes.func,
     placeholder: PropTypes.string,
 };
