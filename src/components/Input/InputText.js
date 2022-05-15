fcdc4bdba92e7f6fe9afd33ba26694911e95a971
Luiz Casara <luiz.casara@maxxidata.com>
2022-05-15 12:46:31 -0300

    feat: create input component

---
+++ src/components/Input/InputText.js
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types';
+import './inputText.css';
+
+export default function InputText({onChange, placeholder}) {
+    return (
+        <div className='input-div'>
+            <input className='input' onChange={onChange} placeholder={placeholder} />
+        </div>
+    )
+}
+
+InputText.propTypes = {
+    onChange: PropTypes.object,
+    placeholder: PropTypes.string,
+};
