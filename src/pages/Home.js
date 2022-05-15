3d260c72a01acca5191a22189330eb0aa5ab45f8
Luiz Casara <luiz.casara@maxxidata.com>
2022-05-15 15:13:44 -0300

    fix: add new load extra data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import react, { useState, useEffect } from 'react';
 import Loading from '../components/Loading/Loading.js';
-import Button from '../components/Button/Button';
-import getUsers from '../resource/GithubResource';
+import Button from '../components/Button/Button.js';
 import logo from '../assets/dock-logo.png';
 import Table from '../components/Table/Table.js';
+import getUsers from '../resource/GithubResource.js';
+import LoadingUserDataByGraphql from '../resource/GithubResourceGraphQl.js';
 import './home.css';
+import '../resource/GithubResourceGraphQl.js'
 
 const TIME_WAITING_LOADING = '1000';
 
@@ -14,7 +16,7 @@ export default function Home() {
     const [usersList, setUsersList] = useState(null);
 
     useEffect(() => {
-        // timer para simular um loading da tela principal ;P 
+        // timer para simular um loading da tela principal
         setTimeout(() => {
             setFirstLoading(false);
         }, TIME_WAITING_LOADING);
@@ -23,11 +25,36 @@ export default function Home() {
     const loadingInitialData = async () => {
         setLoading(true);
         const users = await getUsers();
-
-        if (users != null) {
-            setUsersList(users);
-        }
+        setUsersList(users);
         setLoading(false);
+        // a tela é renderizada com as informações base, mas vai buscar mais dados por user
+        loadMoreDataByUser(users);
+    }
+
+    const loadMoreDataByUser = async (users) => {
+        const promisesList = [];
+        // monta a lista de promises
+        users.forEach(user => {
+            promisesList.push(LoadingUserDataByGraphql(user.login));
+        });
+
+        // executa todas as promise de uma vez
+        const moreUserData = await Promise.all(promisesList)
+            .then((userData) => {
+                return userData;
+            });
+
+        addAdditionalData(users, moreUserData);
+    }
+
+    const addAdditionalData = (users, moreUserData) => {
+        // adicionada os dados novos ao objeto, atualizando a tela
+        const newUsers = [];
+        users.forEach(user => {
+            newUsers.push({ ...user, ...moreUserData.find(item => item?.login === user?.login) });
+        })
+
+        setUsersList(newUsers);
     }
 
     const handleLoadUsers = () => {
@@ -39,25 +66,27 @@ export default function Home() {
     }
 
     function renderButtons() {
-        return(
-            <div className='flex'>
-                <Button handleClick={handleLoadUsers} label="Carregar dados"/>
-                <Button handleClick={handleClearUsers} label="Limpar dados" disabled={usersList == null}/>
+        return (
+            <div className='button-div'>
+                <Button handleClick={handleLoadUsers} label="Carregar dados" />
+                <Button handleClick={handleClearUsers} label="Limpar dados" disabled={usersList == null} />
             </div>
         )
     }
 
     return (
         <div className="main-div">
-            <Loading loading={firstLoading} />
-            {!firstLoading &&
-                <>
-                    <img src={logo} className='App-logo' alt="dock loading logo" />
-                    {renderButtons()}
-                    <Loading loading={loading} />
-                    <Table data={usersList} />
-                </>
-            }
+            <div className="container">
+                <Loading loading={firstLoading} />
+                {!firstLoading &&
+                    <>
+                        <img src={logo} className='App-logo' alt="dock loading logo" />
+                        {renderButtons()}
+                        <Loading loading={loading} />
+                        <Table data={usersList} />
+                    </>
+                }
+            </div>
         </div>
     )
 }
