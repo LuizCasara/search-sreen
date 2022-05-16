23972a1e2522d538a5bfe5f76e88ae898b081c9c
Luiz Casara <luiz.casara@maxxidata.com>
2022-05-16 00:43:18 -0300

    fix: change the key again =/

--- src/resource/GithubResourceGraphQl.js
+++ src/resource/GithubResourceGraphQl.js
@@ -1,7 +1,7 @@
 import { request, gql } from 'graphql-request'
 import getRandomInt from '../helpers/utils';
 
-const AUTH = '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';
+const AUTH = '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';
 const DEFAULT_URL = 'https://api.github.com/graphql';
 const USERS_LOGIN = ["mojombo","defunkt","pjhyett","wycats","ezmobius","ivey","evanphx","vanpelt","wayneeseguin","brynary","kevinclark","technoweenie","macournoyer","takeo","caged","topfunky","anotherjesse","roland","lukas","fanvsfan","tomtt","railsjitsu","nitay","kevwil","Dave","jamesgolick","atmos","errfree","mojodna","bmizerany",];
 
